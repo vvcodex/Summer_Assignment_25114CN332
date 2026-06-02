22660f183d15a4230fe1a29f68525f04ffd59afa
vvcodex <vivek5verma5@gmail.com>
2026-06-02 19:53:08 +0530

    completed week1 day1 Q4

---
+++ week_01/day_01/Q4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num != 0) {
+        num = num / 10;
+        count++;
+    }
+
+    cout << "Number of digits = " << count << endl;
+
+    return 0;
+}
