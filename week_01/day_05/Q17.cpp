25aaa8db6852a5c1e0cda137c93b0dbefedb086c
vvcodex <vivek5verma5@gmail.com>
2026-06-05 20:40:02 +0530

    Q17 added

---
+++ week_01/day_05/Q17.cpp
@@ -0,0 +1,24 @@
+// Write a program to check whether a number is a Perfect Number.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num)
+        cout << num << " is a Perfect Number.";
+    else
+        cout << num << " is not a Perfect Number.";
+
+    return 0;
+}
