d92d0aac23dcff9cf50489ac54495815415c392e
vvcodex <vivek5verma5@gmail.com>
2026-06-08 18:14:18 +0530

    Q33 added

---
+++ week_02/day_02/Q33.cpp
@@ -0,0 +1,20 @@
+// Write a program to print a reverse star pattern.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
