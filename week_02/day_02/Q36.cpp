8363be1b19a389df102349410fee73769855473e
vvcodex <vivek5verma5@gmail.com>
2026-06-11 13:47:24 +0530

    Q36 code uploaded

---
+++ week_02/day_02/Q36.cpp
@@ -0,0 +1,23 @@
+// Write a program to print a hollow square pattern.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows; j++) {
+            if (i == 1 || i == rows || j == 1 || j == rows)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
