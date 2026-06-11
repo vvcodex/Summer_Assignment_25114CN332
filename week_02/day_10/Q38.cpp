80e40b736e13f8648388f8bb51df17f787eb1e59
vvcodex <vivek5verma5@gmail.com>
2026-06-11 14:37:13 +0530

    Q38 uploaded

---
+++ week_02/day_10/Q38.cpp
@@ -0,0 +1,26 @@
+// Write a program to print a reverse pyramid pattern.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = rows; i >= 1; i--) {
+
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
