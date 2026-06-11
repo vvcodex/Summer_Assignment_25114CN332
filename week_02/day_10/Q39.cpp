61b41dd6eb6e5dbde3cac15f0c4776001c61eb43
vvcodex <vivek5verma5@gmail.com>
2026-06-11 14:38:58 +0530

    Q39 code uploaded

---
+++ week_02/day_10/Q39.cpp
@@ -0,0 +1,30 @@
+// Write a program to print a number pyramid pattern.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
