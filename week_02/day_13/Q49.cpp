f2786d24f50fab6487a4f4313bcfc401a5cb7f6c
vvcodex <vivek5verma5@gmail.com>
2026-06-14 05:07:11 +0530

    Q49 code added

---
+++ week_02/day_13/Q49.cpp
@@ -0,0 +1,25 @@
+// Write a program to input and display array elements.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements are: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
