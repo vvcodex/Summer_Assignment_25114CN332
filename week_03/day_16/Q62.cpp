ea85bd633685d5dd2806afaca50fd493a6d09d67
vvcodex <vivek5verma5@gmail.com>
2026-06-20 17:48:28 +0530

    Q62 code completed

---
+++ week_03/day_16/Q62.cpp
@@ -0,0 +1,41 @@
+// Write a program to find the maximum frequency element in an array.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxFreq = 0;
+    int maxElement = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        int count = 1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count > maxFreq) {
+            maxFreq = count;
+            maxElement = arr[i];
+        }
+    }
+
+    cout << "Maximum Frequency Element = " << maxElement << endl;
+    cout << "Frequency = " << maxFreq;
+
+    return 0;
+}
