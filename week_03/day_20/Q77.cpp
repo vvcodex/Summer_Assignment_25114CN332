fbb9a6e3a70cf6c88135aff4f2c50abe32a9d1c0
vvcodex <vivek5verma5@gmail.com>
2026-06-22 21:00:45 +0530

    Q77 added

---
+++ week_03/day_20/Q77.cpp
@@ -0,0 +1,55 @@
+// Write a program to multiply two matrices.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible.";
+        return 0;
+    }
+
+    int A[10][10], B[10][10], C[10][10];
+
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            C[i][j] = 0;
+
+            for (int k = 0; k < c1; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    cout << "Resultant Matrix:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
