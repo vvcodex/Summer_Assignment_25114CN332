62a063e4ae51e7bc3fed9b8b0dc8b3bfba3abe29
vvcodex <vivek5verma5@gmail.com>
2026-06-22 20:57:53 +0530

    Q74 code added

---
+++ week_03/day_19/Q74.cpp
@@ -0,0 +1,43 @@
+// Write a program to subtract two matrices.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+
+    int A[10][10], B[10][10], Result[10][10];
+
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            Result[i][j] = A[i][j] - B[i][j];
+        }
+    }
+
+    cout << "Difference of matrices:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << Result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
