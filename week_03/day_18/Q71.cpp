5de68550c5b49fdf927d1403ad7bd8b4dfdf06c5
vvcodex <vivek5verma5@gmail.com>
2026-06-22 20:54:15 +0530

    Q71 code added

---
+++ week_03/day_18/Q71.cpp
@@ -0,0 +1,46 @@
+// Write a program to perform Binary Search.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter sorted array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1;
+    int position = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            position = mid;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (position != -1)
+        cout << "Element found at index " << position;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
